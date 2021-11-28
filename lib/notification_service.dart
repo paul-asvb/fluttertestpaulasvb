fc5f3f451cf1e2416d80d0b6869d08862b95cf29
Paul Mayer <paul@paul-mayer.eu>
2021-11-28 19:44:45 +0100

    notification init

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -12,4 +12,28 @@ class NotificationService {
   //instance of FlutterLocalNotificationsPlugin
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
+
+  Future<void> init() async {
+    //Initialization Settings for Android
+    final AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+
+    //Initialization Settings for iOS
+    final IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,
+    );
+
+    //InitializationSettings for initializing settings for both platforms (Android & iOS)
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+            android: initializationSettingsAndroid,
+            iOS: initializationSettingsIOS);
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+    );
+  }
 }
