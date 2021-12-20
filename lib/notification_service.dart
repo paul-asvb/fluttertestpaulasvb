6118bb9a93f50b8babccb564df2451da2919f77d
Paul Mayer <paul@paul-mayer.eu>
2021-12-20 20:36:49 +0100

    Init notify

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -36,4 +36,23 @@ class NotificationService {
       initializationSettings,
     );
   }
+
+
+  Future<void> notify() async {
+    //Initialization Settings for Android
+   
+  const AndroidNotificationDetails androidPlatformChannelSpecifics =
+    AndroidNotificationDetails('your channel id', 'your channel name',
+        channelDescription: 'your channel description',
+        importance: Importance.max,
+        priority: Priority.high,
+        ticker: 'ticker');
+const NotificationDetails platformChannelSpecifics =
+    NotificationDetails(android: androidPlatformChannelSpecifics);
+await flutterLocalNotificationsPlugin.show(
+    0, 'plain title', 'plain body', platformChannelSpecifics,
+    payload: 'item x');
+  }
+
+
 }
